0bbf99e294cbe6f42b2017c83d7182c85b2685e8
Obmor <terekhin.alex@mail.ru>
2024-11-08 15:19:19 +0300

    TheSecondLook

--- src/main/java/pro/sky/skyprospringobmorcalculator/CalculatorController.java
+++ src/main/java/pro/sky/skyprospringobmorcalculator/CalculatorController.java
@@ -21,22 +21,50 @@ public class CalculatorController {
     }
 
     @GetMapping(path = "/calculator/plus")
-    public String fivePlusFiveCalculation(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatingService.fivePlusFiveCalculation(num1, num2);
+    public String fivePlusFiveCalculation(@RequestParam(value = "num1", required = false) Integer num1,
+                                          @RequestParam(value = "num2", required = false) Integer num2) {
+        if (num1 == null) {
+            return "U have to enter the data";
+        } else if (num2 == null) {
+            return "U have to enter the data";
+        } else {
+            return calculatingService.fivePlusFiveCalculation(num1, num2);
+        }
     }
 
     @GetMapping(path = "/calculator/minus")
-    public String fiveMinusFiveCalculation(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatingService.fiveMinusFiveCalculation(num1, num2);
+    public String fiveMinusFiveCalculation(@RequestParam(value = "num1", required = false) Integer num1,
+                                           @RequestParam(value = "num2", required = false) Integer num2) {
+        if (num1 == null) {
+            return "U have to enter the data";
+        } else if (num2 == null) {
+            return "U have to enter the data";
+        } else {
+            return calculatingService.fiveMinusFiveCalculation(num1, num2);
+        }
     }
 
     @GetMapping(path = "/calculator/multiply")
-    public String fiveMultiplyFiveCalculation(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatingService.fiveMultiplyFiveCalculation(num1, num2);
+    public String fiveMultiplyFiveCalculation(@RequestParam(value = "num1", required = false) Integer num1,
+                                              @RequestParam(value = "num2", required = false) Integer num2) {
+        if (num1 == null) {
+            return "U have to enter the data";
+        } else if (num2 == null) {
+            return "U have to enter the data";
+        } else {
+            return calculatingService.fiveMultiplyFiveCalculation(num1, num2);
+        }
     }
 
     @GetMapping(path = "/calculator/divide")
-    public String fiveDivideFiveCalculation(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatingService.fiveDivideFiveCalculation(num1, num2);
+    public String fiveDivideFiveCalculation(@RequestParam(value = "num1", required = false) Integer num1,
+                                            @RequestParam(value = "num2", required = false) Integer num2) {
+        if (num1 == null) {
+            return "U have to enter the data";
+        } else if (num2 == null) {
+            return "U have to enter the data";
+        } else {
+            return calculatingService.fiveDivideFiveCalculation(num1, num2);
+        }
     }
 }
